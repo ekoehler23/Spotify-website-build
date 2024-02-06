bae194b26738fd1767b32c50985409ea2f51379e
e.koehler23@gmail.com <e.koehler23@gmail.com>
2024-02-06 01:01:13 +0000

    Edited Frame and Reports

--- client_code/Reports/__init__.py
+++ client_code/Reports/__init__.py
@@ -1,45 +1,53 @@
 from ._anvil_designer import ReportsTemplate
 from anvil import *
-import plotly.graph_objects as go
 import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import plotly.graph_objects as go
 
 class Reports(ReportsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
+    #The x-axis of plot_1 will be the months of the year. The y-axis will be dummy data returned from the server
+    self.x_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    #Get the y-values from the server
+    self.y_values = anvil.server.call('return_data', "2023")
+    self.create_line_graph()
 
-    #Populate plot_1 with dummy data. All three Bar charts will be added to the same figure
-    self.plot_1.data = [
-      go.Bar(
-        x=[2019, 2020, 2021, 2022, 2023],
-        y=[510, 620, 687, 745, 881],
-        name="Europe"
-    ),
-      go.Bar(
-        x=[2019, 2020, 2021, 2022, 2023],
-        y=[733, 880, 964, 980, 1058],
-        name="Americas"
-    ),
-      go.Bar(
-        x=[2019, 2020, 2021, 2022, 2023],
-        y=[662, 728, 794, 814, 906],
-        name="Asia"
-    )
-    ]
-
-    #Return the figure from the server to populate plot_2
-    self.plot_2.figure = anvil.server.call('return_bar_charts')
-
-    self.plot_3.data = [
+    self.plot_2.data = [
       go.Pie(
-        labels=["Mobile", "Tablet", "Desktop"],
-        values=[2650, 755, 9525]
+        labels=["Completed", "In progress", "Needs Review", "Not started"],
+        values=[30, 21, 10, 39 ],
+        hole=.5
       )
     ]
-    
 
+    #Set the contents of the data grid (which has a repeating panel inside) to the contents of
+    #the Files table. This is done on the secure server where you might only want to return user-visible data
+    self.repeating_panel_1.items = anvil.server.call('return_table')
+
+  def create_line_graph(self):
+    self.plot_1.data = [
+      go.Scatter(
+        x=self.x_months,
+        y=self.y_values[0],
+        fill="tozeroy",
+        name="Product A"
+      ),
+        go.Scatter(
+        x=self.x_months,
+        y=self.y_values[1],
+        fill="tonexty",
+        name="Product B"
+      )
+    ]
+
+  #Update the values in the line graph based on the selected value of the drop down menu
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self.y_values = anvil.server.call('return_data', self.drop_down_1.selected_value)
+    self.create_line_graph()
